87528638637d007ee7c39efc417c09b1401b787b
Mark-42-Max <publicsoham.24@gmail.com>
2021-06-14 14:17:28 +0100

    Crowd behavior initiated

--- Count Masters/Assets/Scripts/PlayerManager.cs
+++ Count Masters/Assets/Scripts/PlayerManager.cs
@@ -1,25 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class PlayerManager : MonoBehaviour
 {
-    public GameObject[] player;  //for player
-    
-    Rigidbody[] rb;     //for deetcting each collision
+    public GameObject[] player;  //for initial character
+    private List<GameObject> army = new List<GameObject>();  //for army
+
+    Transform place;
+
+
+    GameObject go;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        GameObject go;
-        go = Instantiate(player[0], transform);
-        go.AddComponent<Rigidbody>();
+        go = Instantiate(player[0]);
+        go.transform.SetParent(transform);
+        army.Add(go);
+        var position = new Vector3(Random.Range(-10, 10), 1.51f, Random.Range(-10, 10));
+        go = Instantiate(player[0], position, Quaternion.identity);
+        go.transform.SetParent(transform);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.forward * 5 * Time.deltaTime);
     }
 }
